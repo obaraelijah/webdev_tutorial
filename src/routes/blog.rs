4fd78c6cfb567234bc2fae868a6f9539a99cd9c9
obaraelijah <elijahobara357@gmail.com>
2024-03-06 17:07:20 +0300

    feat: Implemented endpoint to create a new blog post and insert it into the database

---
+++ src/routes/blog.rs
@@ -0,0 +1,52 @@
+use crate::dtypes::structs::{Blog, Id};
+use crate::db;
+use crate::utils::handle_sql_error;
+use actix_web::http::StatusCode;
+use actix_web::{get, post, put, delete, web::Query, web::Json, HttpResponse, web};
+use sqlx::postgres::PgQueryResult;
+use sqlx::Error;
+
+
+#[post("/blog")]
+async fn create_blog(blog: Json<Blog>) -> HttpResponse {
+    match db::connect().await {
+        Ok(pg) => {
+            let returned = sqlx::query_as!(
+                Blog,
+                r#"
+                INSERT INTO blog (title, slug, content, image_link, thumbnail_link, featured)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING id, title, slug, content, image_link, thumbnail_link, featured,
+                    to_char(created, 'DD Month YYYY HH12:MI AM') as created,
+                    to_char(edited, 'DD Month YYYY HH12:MI AM') as edited
+                "#,
+                blog.title, 
+                blog.slug,
+                blog.content,
+                blog.image_link,
+                blog.thumbnail_link,
+                blog.featured
+            )
+            .fetch_one(&pg)
+            .await;
+
+            match returned {
+                Ok(record) => HttpResponse::Created()
+                    .status(StatusCode::CREATED)
+                    .content_type("application/json")
+                    .body(
+                        serde_json::to_string(&Json(record))
+                            .unwrap_or_else(|e| format!("JSON serialization error: {}", e)),
+                    ),
+
+                Err(e) => handle_sql_error(e),
+            }
+        }
+        Err(e) => HttpResponse::InternalServerError()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .content_type("application/json")
+            .body(e.message)
+    }
+}
+
+
