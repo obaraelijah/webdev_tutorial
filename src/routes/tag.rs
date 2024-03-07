f466ee7c6bed9c210fabbfc417c764c913b96ae3
obaraelijah <elijahobara357@gmail.com>
2024-03-07 14:37:32 +0300

    feat: iimpliment CRUD logic forn tagging system

--- src/routes/tag.rs
+++ src/routes/tag.rs
@@ -1,10 +1,12 @@
+use std::future::IntoFuture;
+
 use crate::dtypes::structs::{ Id, Tag, AssocTable, TagQueryParams};
-use  crate::db;
+use crate::db;
 use crate::utils::handle_sql_error;
 use actix_web::http::StatusCode;
 use actix_web::{get, put, post, delete, web::Query, HttpResponse, web::Json};
 use sqlx::postgres::PgQueryResult;
-use sqlx::{query, Error};
+use sqlx::Error;
 
 #[post("/tag")]
 async fn create_tag(tag: Json<Tag>) -> HttpResponse {
@@ -211,4 +213,75 @@ async fn get_tag_by_id_or_all(Query(params): Query<TagQueryParams>) -> HttpRespo
             }
         }
     }
+}
+
+#[put("/blog")]
+async fn update_slug(tag: Json<Tag>) -> HttpResponse {
+    match db::connect().await {
+        Ok(pg) => {
+            let returned: Result<Tag, Error> = sqlx::query_as!(
+                Tag,
+                r#"
+                    INSERT INTO tag (id, name, assoc_table)
+                    VALUES ($1, $2, $3)
+                    ON_CONFLICT (id)
+                    DO UPDATE SET
+                        id = EXCLUDED.id,
+                        name = EXCLUDED.name,
+                        assoc_table = EXCLUDED.assoc_table,
+                        edited = now()
+                    RETURNING
+                        id,
+                        name,
+                        assoc_table as "assoc_table: AssocTable",
+                        to_char(created, 'DD Month YYYY HH12:MI AM') as created,
+                        to_char(edited, 'DD Month YYYY HH12:MI AM') as edited
+                "#,
+                tag.id,
+                tag.name,
+                tag.assoc_table as AssocTable,
+            )
+            .fetch_one(&pg)
+            .await;
+
+            match returned {
+                Ok(record) => HttpResponse::Ok()
+                    .status(StatusCode::OK)
+                    .content_type("application/json")
+                    .body(
+                        serde_json::to_string(&Json(record))
+                            .unwrap_or_else(|e| format!("JSON serialization error: {}", e)),
+                    ),
+                Err(e) => handle_sql_error(e),  
+            }
+        }
+        Err(e) => HttpResponse::InternalServerError()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .content_type("application/json")
+            .body(e.message),
+    }
+}
+
+#[delete("/tag")]
+async fn delete_tag(id: Json<Id>) -> HttpResponse {
+    match db::connect().await {
+        Ok(pg) => {
+            let returned: Result<PgQueryResult, Error> =    
+                sqlx::query_as!(Tag, "DELETE FROM Tag WHERE id = $1;", id.id)
+                    .execute(&pg)
+                    .await;
+            match returned {
+                Ok(_) => HttpResponse::NoContent()
+                    .status(StatusCode::NO_CONTENT)
+                    .content_type("application/json")
+                    .finish(),
+
+                Err(e) => handle_sql_error(e),
+            }
+        }
+        Err(e) => HttpResponse::InternalServerError()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .content_type("application/json")
+            .body(e.message)
+        }
 }
